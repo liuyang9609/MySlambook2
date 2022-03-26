36f371f6a24d383b510d61b32e4597073853f88b
liuyang9609 <ly_0913@foxmail.com>
2022-03-26 23:29:53 +0800

    update

---
+++ MySlambook2/ch13/gx_tinySLAM/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/liuyang/MySLAM/SLAMProgramming/MySlambook2/ch13/gx_tinySLAM
+# Build directory: /home/liuyang/MySLAM/SLAMProgramming/MySlambook2/ch13/gx_tinySLAM/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
---
+++ MySlambook2/ch13/gx_tinySLAM/cmake-build-debug/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/liuyang/MySLAM/SLAMProgramming/MySlambook2/ch13/gx_tinySLAM/src
+# Build directory: /home/liuyang/MySLAM/SLAMProgramming/MySlambook2/ch13/gx_tinySLAM/cmake-build-debug/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
